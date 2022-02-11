a50223ce5868f89229796e0a73ef7f5ebc999b5e
Anilkumar <maddianilkumar160801@gmail.com>
2022-02-11 16:17:29 +0530

    Done

---
+++ 3_Implementation/src/main.c
@@ -0,0 +1,49 @@
+#include"booking_ticket.h"
+void main()
+{
+	login();
+	int ch;
+	while(1)
+	{
+		printf("\n ********************************************");
+		printf("\n");
+		printf("\t MOVIE TICKET BOOKING ");
+		printf("\n");
+		printf("\n ********************************************");
+		
+		printf("\n Enter >1< To insert Movie");
+		printf("\n Enter >2< To View All Movies");
+		printf("\n Enter >3< To Find Movie");
+		printf("\n Enter >4< To Book Tickets");
+		printf("\n Enter >5< To View All Transactions");
+		printf("\n Enter >0< To Exit");
+		
+		printf("\n Enter your Choice ::");
+		scanf("%d", &ch);
+		
+		switch (ch)
+		{
+			case 1 :
+				insert_details();
+			break;
+			case 2 :
+				viewAll();
+			break;
+			case 3 :
+				find();
+			break;
+			case 4 :
+				book_ticket();
+			break;
+			case 5 :
+				old_record();
+			break;
+			case 0:
+				exit(0);
+			break;
+			default :
+				printf("Wrong Choice !");
+			break;			 
+		}
+	}while(ch!=0);
+}
