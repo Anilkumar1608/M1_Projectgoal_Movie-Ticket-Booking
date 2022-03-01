22c1c1966d05ee3557254b6fcc461a1d82b05a3e
Anilkumar <maddianilkumar160801@gmail.com>
2022-03-01 13:18:44 +0530

    Done

---
+++ 3_Implementation/src/booking_ticket.h
@@ -0,0 +1,25 @@
+/**
+ * @file booking_ticket.c
+ * @author your name (you@domain.com)
+ * @brief movie booking tickets
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef booking_ticket_H_
+#define booking_ticket_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void login();
+void insert_details();
+void viewAll();
+void find();
+void book_ticket();
+void old_record();
+
+#endif
--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -8,6 +8,7 @@
  * @copyright Copyright (c) 2022
  * 
  */
+
 #include"booking_ticket.h"
 void main()
 {
